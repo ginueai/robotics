620a7acfd3d1930004b8d05e78006a8997907d91
Hyoung-Yong Park <wlghfkr4ever@gmail.com>
2021-08-03 12:20:52 +0900

    debug

--- led_simple.py
+++ led_simple.py
@@ -5,7 +5,7 @@ pixels = neopixel.NeoPixel(board.D18, 8)
 pixels[0] = (255, 0, 0)
 pixels[2] = (0, 0, 255)
 pixels[4] = (255, 255, 0)
-pixels[5] = (0, 255.0)
+pixels[5] = (0, 255, 0)
 
 
 pixels.show
